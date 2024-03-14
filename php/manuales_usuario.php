a23b84da06649af8cd31fc29403d77ea02396072
JesusJ4s <jesusgole33@gmail.com>
2024-03-14 00:47:29 -0400

    agregando manuales

--- php/manuales_usuario.php
+++ php/manuales_usuario.php
@@ -5,11 +5,11 @@ function manuales(){
     if ($_SESSION['nivel_usuario'] == 1) {
         echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioAdministrador.pdf' target='_blank'>Manual de Usuario</a></li>";
     }else if ($_SESSION['nivel_usuario'] == 2) {
-        // echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioAdministrador.pdf' target='_blank'>Manual de Usuario</a></li>";
+        echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioIngenieroInformatico.pdf' target='_blank'>Manual de Usuario</a></li>";
     }else if ($_SESSION['nivel_usuario'] == 3) {
-        // echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioAdministrador.pdf' target='_blank'>Manual de Usuario</a></li>";
+        echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioJefeDepartamento.pdf' target='_blank'>Manual de Usuario</a></li>";
     }else if ($_SESSION['nivel_usuario'] == 4) {
-        // echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioAdministrador.pdf' target='_blank'>Manual de Usuario</a></li>";
+        echo "<li><a class='dropdown-item' href='../assets/intranet/manuales/ManualdeUsuarioEmpleado.pdf' target='_blank'>Manual de Usuario</a></li>";
     } 
 
 }
